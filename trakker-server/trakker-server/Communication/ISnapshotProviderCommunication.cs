4e0c5465485fb71cd1b1f1da5871a642c34a8cd1
dvirpla <dvirpla@gmail.com>
2020-03-31 20:12:08 +0300

    HDT-19 Added communication with snapshot provider

---
+++ trakker-server/trakker-server/Communication/ISnapshotProviderCommunication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrakkerModels;
+
+namespace TrakkerServer.Communication
+{
+    public interface ISnapshotProviderCommunication
+    {
+        List<System.IO.DriveInfo> GetDrivesMetadata();
+
+        DriveInfo GetDriveInfo(string driveName);
+
+    }
+}
---
+++ trakker-server/trakker-server/Communication/LocalSnapshotProviderCommunication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SnapshotProvider;
+
+namespace TrakkerServer.Communication
+{
+    public class LocalSnapshotProviderCommunication : ISnapshotProviderCommunication
+    {
+        public LocalSnapshotProviderCommunication(ISnapshotProvider snapshotProvider)
+        {
+            this.SnapshotProvider = snapshotProvider;
+        }
+
+        private ISnapshotProvider SnapshotProvider { get; }
+
+        public List<DriveInfo> GetDrivesMetadata()
+        {
+            return this.SnapshotProvider.GetDrivesMetadata();
+        }
+
+        public TrakkerModels.DriveInfo GetDriveInfo(string driveName)
+        {
+            return this.SnapshotProvider.GetDriveInfo(driveName);
+        }
+    }
+}
