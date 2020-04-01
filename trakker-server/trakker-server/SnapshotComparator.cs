cff3893de0cf993aeb801f2547050844d97e475a
dvirpla <dvirpla@gmail.com>
2020-04-01 15:22:33 +0300

    HDT-24 Empty function

---
+++ trakker-server/trakker-server/SnapshotComparator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TrakkerModels;
+
+namespace TrakkerServer
+{
+    public class SnapshotComparator
+    {
+
+        public void CompareTwoDirectories(TrakkerModels.DirectoryInfo dirOne, TrakkerModels.DirectoryInfo dirTwo)
+        {
+
+        }
+    }
+}
